d52629eeb0c26c30aef459da7238583feadbd30b
DaniRainbow <jordanavery02@gmail.com>
2025-02-15 11:07:29 -0700

    changes to party_menu.c to fix Fly showing as Rock Smash

--- src/party_menu.c
+++ src/party_menu.c
@@ -2542,7 +2542,7 @@ static void SetPartyMonFieldSelectionActions(struct Pokemon *mons, u8 slotId)
             if (GetMonData(&mons[slotId], i + MON_DATA_MOVE1) == sFieldMoves[j])
             {
                 // If Mon already knows FLY and the HM is in the bag, prevent it from being added to action list
-                if (sFieldMoves[j] != MOVE_FLY || !CheckBagHasItem(ITEM_HM02_FLY, 1)){
+                if (sFieldMoves[j] != FIELD_MOVE_FLY || !CheckBagHasItem(ITEM_HM02_FLY, 1)){
                     // If Mon already knows FLASH and the HM is in the bag, prevent it from being added to action list
                     if (sFieldMoves[j] != MOVE_FLASH || !CheckBagHasItem(ITEM_HM05_FLASH, 1)){ 
                         AppendToList(sPartyMenuInternal->actions, &sPartyMenuInternal->numActions, j + MENU_FIELD_MOVES);
@@ -4693,7 +4693,7 @@ int MoveToHM(u16 move)
     case MOVE_CUT:
         item = ITEM_HM01;
         break;
-    case MOVE_FLY:
+    case FIELD_MOVE_FLY:
         item = ITEM_HM02;
         break;
     case MOVE_SURF:
