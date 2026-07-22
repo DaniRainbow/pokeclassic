6f8e1999590a0dcc94691dddb6b5362536e8d465
DaniRainbow <jordanavery02@gmail.com>
2026-07-22 16:30:54 -0600

    Charizard no longer has ghost claws and introduced a mitigation for player leaving Rocket Hideout after Jessie and James move

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -885,7 +885,7 @@
 #define FLAG_GOT_RED_CARD     									    0x346
 #define FLAG_HIDE_SCOTT_AT_HOUSE                           		    0x347
 #define FLAG_OAK_ADVENTURE_AWAITS                      				0x348
-#define FLAG_HIDE_UNUSED_115               							0x349
+#define FLAG_JESSIE_JAMES_READY              						0x349
 #define FLAG_HIDE_UNUSED_116                       					0x34A
 #define FLAG_HIDE_UNUSED_117                   						0x34B
 #define FLAG_HIDE_UNUSED_118                           				0x34C
